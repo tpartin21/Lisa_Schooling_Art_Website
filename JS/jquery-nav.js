feafdc01b5238e0aee3b2b011330dd311908819f
Thea Partin <thea.riley.partin@gmail.com>
2021-03-30 19:15:16 -0400

    update project

--- JS/jquery-nav.js
+++ JS/jquery-nav.js
@@ -21,3 +21,80 @@ function clickHandler(e) {
     behavior: "smooth"
   });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  fields.firstName = document.getElementById('firstName');
+  fields.lastName = document.getElementById('lastName');
+  fields.email = document.getElementById('email');
+  fields.address = document.getElementById('address');
+  fields.houseNumber = document.getElementById('houseNumber');
+  fields.country = document.getElementById('country');
+  fields.password = document.getElementById('password');
+  fields.passwordCheck = document.getElementById('passwordCheck');
+  fields.newsletter = document.getElementById('newsletter');
+  fields.question = document.getElementById('question');
+ })
+
+ function isNotEmpty(value) {
+  if (value == null || typeof value == 'undefined' ) return false;
+  return (value.length > 0);
+ }
+
+ function isNumber(num) {
+  return (num.length > 0) && !isNaN(num);
+ }
+
+ function isEmail(email) {
+   let regez =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/; 
+   return regez.test(String(email).toLowerCase());
+ }
+
+ function fieldValidation(field, validationFunction) {
+  if (field == null) return false;
+ 
+  let isFieldValid = validationFunction(field.value)
+  if (!isFieldValid) {
+  field.className = 'placeholderRed';
+  } else {
+  field.className = '';
+  }
+ 
+  return isFieldValid;
+ }
+ function isValid() {
+  var valid = true;
+  
+  valid &= fieldValidation(fields.firstName, isNotEmpty);
+  valid &= fieldValidation(fields.lastName, isNotEmpty);
+  valid &= fieldValidation(fields.gender, isNotEmpty);
+  valid &= fieldValidation(fields.address, isNotEmpty);
+  valid &= fieldValidation(fields.country, isNotEmpty);
+  valid &= fieldValidation(fields.email, isEmail);
+  valid &= fieldValidation(fields.houseNumber, isNumber);
+  valid &= fieldValidation(fields.question, isNotEmpty);
+  valid &= arePasswordsEqual();
+ 
+  return valid;
+ }
+ class User {
+  constructor(firstName, lastName, gender, address, country, email, newsletter, question) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.gender = gender;
+  this.address = address;
+  this.country = country;
+  this.email = email;
+  this.newsletter = newsletter;
+  this.question = question;
+  }
+ }
+
+ function sendContact (){
+   if (isValid()) {
+     let usr = new User(firstName.value, lastName.value, address.value, country.value, email.value, newsletter.checked);
+
+     alert (`${usr.firstName} Thanks for registering.`)
+} else {
+  alert("There was an error")
+} 
+ }
